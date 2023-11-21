4d6a40889192a33100c5bbb445f5bde822705b38
Yurcik Diachenko <yurcik45mail@gmail.com>
2023-11-21 22:01:26 +0200

    add final logic to MainActivity

--- app/src/main/java/com/example/android_todo/MainActivity.kt
+++ app/src/main/java/com/example/android_todo/MainActivity.kt
@@ -2,11 +2,41 @@ package com.example.android_todo
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : ComponentActivity() {
+    private lateinit var recyclerViewTodoItems: RecyclerView
+    private lateinit var buttonAddTodo: Button
+    private lateinit var buttonDeleteAllDone: Button
+    private lateinit var editTextTodoItem: EditText
+    private lateinit var todoAdapter: TodoAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        todoAdapter = TodoAdapter(mutableListOf())
+
+        recyclerViewTodoItems = findViewById(R.id.recyclerViewTodoItems)
+        buttonAddTodo = findViewById(R.id.buttonAddTodo)
+        buttonDeleteAllDone = findViewById(R.id.buttonDeleteAllDone)
+        editTextTodoItem = findViewById(R.id.editTextTodoItem)
+
+        recyclerViewTodoItems.adapter = todoAdapter
+        recyclerViewTodoItems.layoutManager = LinearLayoutManager(this)
+
+        buttonAddTodo.setOnClickListener {
+            val todoTitle = editTextTodoItem.text.toString()
+            if (todoTitle.isNotEmpty()) {
+                val todo = Todo(todoTitle)
+                todoAdapter.addTodo(todo)
+                editTextTodoItem.text.clear()
+            }
+        }
+        buttonDeleteAllDone.setOnClickListener {
+            todoAdapter.deleteDoneTodos()
+        }
     }
 }
 
